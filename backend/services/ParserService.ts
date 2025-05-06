784e3a9eb11ad4f4849b870eb5f8f41e3ad167b2
armynion <army.brunet@laposte.net>
2025-05-06 22:18:34 +0200

    refactor(parsing): refactoring ParserService for readability

--- backend/services/ParserService.ts
+++ backend/services/ParserService.ts
@@ -1,54 +1,82 @@
-import { GraduatedStudent, Guest } from '../business/Objects';
+import { GraduatedStudent, Guest } from "../business/Objects";
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from "csv-parse";
 
-import * as fs from 'fs';
-import * as path from 'path';
-import { parse } from 'csv-parse';
+export type ColumnsNames = {
+  firstName: string;
+  lastName: string;
+  buyerfirstName: string;
+  buyerlastName: string;
+  buyerEmail: string;
+  diet: string;
+  wantedTableMates: string;
+};
+
+type ParsedCSVRow = Record<string, string>;
+
+type GraduatedStudentMap = Map<string, GraduatedStudent>;
 
 export class ParserService {
-  static graduatedStudent: GraduatedStudent[] = [];
+  private static columns: ColumnsNames = {
+    firstName: "Prenom",
+    lastName: "Nom",
+    buyerfirstName: "Prenom acheteur",
+    buyerlastName: "Nom acheteur",
+    buyerEmail: "E-mail acheteur",
+    diet: "Regime alimentaire #131474",
+    wantedTableMates: "Avec qui voulez-vous manger? (commande) #135122",
+  };
 
-  static async readFileCSV(csvFilePath: string): Promise<void> {
+  static async readFileCSV(csvFilePath: string): Promise<GraduatedStudent[]> {
     const absolutePath = path.resolve(csvFilePath);
-    const graduates: Map<string, GraduatedStudent> = new Map();
-  
+    const graduatedStudents: GraduatedStudentMap = new Map();
+    
     return new Promise((resolve, reject) => {
       fs.createReadStream(absolutePath)
-        .pipe(parse({ delimiter: ';', columns: true, trim: true, bom:true}))
-        .on('data', (row: any) => {
-          const guestName = row['Nom'].trim();
-          const guestFirstName = row['Prenom'].trim();
-          const graduateName = row['Nom acheteur'].trim();
-          const graduateFirstName = row['Prenom acheteur'].trim();
-          const diet = row['Regime alimentaire #131474'].trim();
+        .pipe(parse({ delimiter: ";", columns: true, trim: true, bom: true }))
+        .on("data", (row: ParsedCSVRow) => {
+          const {
+            firstName,
+            lastName,
+            buyerfirstName,
+            buyerlastName,
+            buyerEmail,
+            diet,
+            wantedTableMates,
+          } = ParserService.columns;
 
-          const graduateKey = graduateFirstName + ' ' + graduateName;
+          const guestFirstName = row[firstName]?.trim();
+          const guestLastName = row[lastName]?.trim();
+          const gradFirstName = row[buyerfirstName]?.trim();
+          const gradLastName = row[buyerlastName]?.trim();
+          const gradEmail = row[buyerEmail]?.trim();
+          const tableMates = row[wantedTableMates];
+          const specifiedDiet = row[diet]?.trim();
 
-          if (guestName !== graduateName || guestFirstName !== graduateFirstName) {
-            if (!graduates.has(graduateKey)) {
-              const email = row['E-mail acheteur'].trim();
-              const neighbours = row['Avec qui voulez-vous manger? (commande) #135122'].trim();
-              graduates.set(graduateKey, new GraduatedStudent(graduateName, graduateFirstName, email, neighbours));
-            }
-            graduates.get(graduateKey)!.addGuest(new Guest(guestName, guestFirstName, diet));
-          } else {
-            if (!graduates.has(graduateKey)) {
-              const email = row['E-mail acheteur'].trim();
-              const neighbours = row['Avec qui voulez-vous manger? (commande) #135122'].trim();
-              graduates.set(graduateKey, new GraduatedStudent(graduateName, graduateFirstName, email, neighbours));
-            }
-            graduates.get(graduateKey)!.setDiet(diet);
+          const gradKey = `${gradFirstName} ${gradLastName}`;
+
+          const guestIsGraduatedStudent = guestFirstName === gradFirstName && guestLastName === gradLastName;
+
+          if (!graduatedStudents.has(gradKey)) {
+            graduatedStudents.set(
+              gradKey,
+              new GraduatedStudent(gradLastName, gradFirstName, gradEmail, tableMates)
+            );
           }
+
+          if (!guestIsGraduatedStudent) {
+            const guest = new Guest(guestLastName, guestFirstName, specifiedDiet);
+            graduatedStudents.get(gradKey)!.addGuest(guest);
+          }
+          graduatedStudents.get(gradKey)!.setDiet(specifiedDiet);
         })
-        .on('end', () => {
-          ParserService.graduatedStudent = Array.from(graduates.values());
-          resolve();
-        })
-        .on('error', (error) => {
-          reject(error);
-        });
+        .on("end", () => resolve(Array.from(graduatedStudents.values())))
+        .on("error", reject);
     });
   }
-  static async getGraduatedStudent(): Promise<GraduatedStudent[]> {
-    return this.graduatedStudent;
+
+  static setColumnsNames(columns: ColumnsNames): void {
+    this.columns = columns;
   }
 }
