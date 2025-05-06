9bd44ce3a55da2011b1f62b02942506c90568242
AO2708 <a.pitardbouet@gmail.com>
2025-05-06 16:18:37 +0200

    First version of parser

--- backend/services/ParserService.ts
+++ backend/services/ParserService.ts
@@ -4,32 +4,48 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { parse } from 'csv-parse';
 
-export class ReaderService {
-  static async readFileCSV(csvFilePath: string): Promise<GraduatedStudent[]> {
+export class ParserService {
+  static graduatedStudent: GraduatedStudent[] = [];
+
+  static async readFileCSV(csvFilePath: string): Promise<void> {
     const absolutePath = path.resolve(csvFilePath);
-    const students: GraduatedStudent[] = [];
-
-    
-
+    const graduates: Map<string, GraduatedStudent> = new Map();
+  
     return new Promise((resolve, reject) => {
       fs.createReadStream(absolutePath)
-        .pipe(parse({ delimiter: ',', columns: true, trim: true }))
+        .pipe(parse({ delimiter: ';', columns: true, trim: true, bom:true}))
         .on('data', (row: any) => {
-          const student = new GraduatedStudent(
-            row.lastName,
-            row.firstName,
-            row.email,
-            parseInt(row.nbGuest),
-            parseInt(row.nbNeighbour)
-          );
-          students.push(student);
+          const guestName = row['Nom'].trim();
+          const guestFirstName = row['Prenom'].trim();
+          const graduateName = row['Nom acheteur'].trim();
+          const graduateFirstName = row['Prenom acheteur'].trim();
+
+          const graduateKey = graduateFirstName + ' ' + graduateName;
+
+          if (guestName !== graduateName || guestFirstName !== graduateFirstName) {
+            if (!graduates.has(graduateKey)) {
+              const email = row['E-mail acheteur'].trim();
+              graduates.set(graduateKey, new GraduatedStudent(graduateName, graduateFirstName, email));
+            }
+            const diet = row['Regime alimentaire #131474'].trim();
+            graduates.get(graduateKey)!.addGuest(new Guest(guestName, guestFirstName, diet));
+          } else {
+            if (!graduates.has(graduateKey)) {
+              const email = row['E-mail acheteur'].trim();
+              graduates.set(graduateKey, new GraduatedStudent(graduateName, graduateFirstName, email));
+            }
+          }
         })
         .on('end', () => {
-          resolve(students);
+          ParserService.graduatedStudent = Array.from(graduates.values());
+          resolve();
         })
         .on('error', (error) => {
           reject(error);
         });
     });
+  }
+  static async getGraduatedStudent(): Promise<GraduatedStudent[]> {
+    return this.graduatedStudent;
   }
 }
