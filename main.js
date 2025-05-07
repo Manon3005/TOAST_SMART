d7fd676d029cc33a60845c77a3e154228d122a93
BugaThis <mathis.getenet@insa-lyon.fr>
2025-05-07 11:18:12 +0200

    Window size

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const { ParserService } = require("./backend/dist/backend/services/ParserService.js");
 const { GraduatedStudent } = require("./backend/dist/backend/business/Objects.js")
 const path = require('path')
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, screen } = require('electron/main')
 const { ipcMain, dialog } = require('electron');
 
 async function createWindow() {
+
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: Math.floor(width * 0.6),    
+    height: Math.floor(height * 0.8),    
+    minWidth: 800,                       
+    minHeight: 600,
     webPreferences: {
       contextIsolation: true,
       preload: path.join(__dirname, 'preload.js'),
@@ -50,6 +55,10 @@ async function createWindow() {
   } catch (error) {
     console.error("Erreur lors du traitement du fichier CSV:", error);
   }
+
+  win.setMenu(null);
+  win.center();
+  win.setResizable(true);
 
   win.loadFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   //win.webContents.openDevTools();  // pour debogage
