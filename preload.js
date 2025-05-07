da65f2a8de9764d831d56fc83885867a7a31113f
AO2708 <a.pitardbouet@gmail.com>
2025-05-07 16:48:58 +0200

    Adding of a service

--- preload.js
+++ preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 // On expose une API sécurisée dans `window.electronAPI`
 contextBridge.exposeInMainWorld('electronAPI', {
   pickFile: () => ipcRenderer.invoke('dialog:openFile'),
-  generateTablePlan: (jsonData) => ipcRenderer.invoke('dialog:generateTablePlan')
+  parseCsvFile: () => ipcRenderer.invoke('dialog:beginCsvParsing'),
+  generateTablePlan: (jsonData) => ipcRenderer.invoke('dialog:generateTablePlan'),
 });
