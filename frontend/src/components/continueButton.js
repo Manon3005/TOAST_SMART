6f1c8e11ec9a25f1eea651fb8922548d05e702b2
AUB <roche.aubin@gmail.com>
2025-05-08 12:01:22 +0200

    button continue reset and inputNumbers (juste front logic no link with back)

---
+++ frontend/src/components/continueButton.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+
+
+export function ContinueButton({onClick}) {
+
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'button',
+        { onClick : onClick, className: 'file-button' },'Valider' )
+);
+}
