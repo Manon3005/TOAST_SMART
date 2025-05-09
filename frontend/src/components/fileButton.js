74e76d0444b71cb7c16d0f346efc2870d7587d53
AUB <roche.aubin@gmail.com>
2025-05-09 11:30:55 +0200

    dropdowns + recuperation headers csv

--- frontend/src/components/fileButton.js
+++ frontend/src/components/fileButton.js
@@ -3,35 +3,14 @@ import '../App.css';
 
 
 
-export function FileButton({disabled, nameFile, setName, errorFile, setErrorFile}) {
-    //Variable Initilization
-    const [filePath, setPath] = useState(''); 
-    
-    //fonction to load a file and get his path
-    const loadFile = async () => {
-        try {
-            const filePath = await window.electronAPI.pickFile();
-            if (!filePath.endsWith('.csv')) {
-                setErrorFile('Veuillez sélectionner un fichier .csv');
-                return;
-            }
-            
-            const name = filePath.split(/[/\\]/).pop();
-            setName(name);
-            setPath(filePath);
-            setErrorFile('');
-        } catch (err) {
-            setErrorFile('Erreur chargement du fichier');
-            setPath('');
-    }
-  };
+export function FileButton({disabled, nameFile, setName, errorFile, setErrorFile, onClick}) {
 
   return React.createElement(
     'div',
     null,
     React.createElement(
         'button',
-        { onClick: loadFile, className: 'file-button', disabled : disabled},'📁 Charger un fichier' ),
+        { onClick: onClick, className: 'file-button', disabled : disabled},'📁 Charger un fichier' ),
     errorFile && React.createElement('p', { style: { color: 'red' } }, errorFile),
     nameFile && React.createElement('p', null, '📄 ' + nameFile)
 );
