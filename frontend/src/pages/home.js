f79adc39420eebcd38ff940153ed63f2fe0ddae6
AUB <roche.aubin@gmail.com>
2025-05-09 09:37:22 +0200

    elements creation (conflictcenter, buttons)

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,8 +3,10 @@ import { InputNumber } from "../components/inputNumber";
 import { ResetButton } from "../components/resetButton";
 import { ContinueButton } from "../components/continueButton";
 import { TableColumn } from "../components/tableColumn";
+import { ConflictCenter } from "../components/conflictCenter";
 import React, { useState } from 'react';
 import '../App.css';
+import { GenerateButton } from "../components/generateButton";
 
 
 export function Home() {
@@ -15,15 +17,26 @@ export function Home() {
     const [maxGuests, setMaxGuests] = useState(1);
     const [nameFile, setName] = useState('');
     const [errorFile, setErrorFile] = useState('');
-    const [locked, setLocked] = useState(false);
+    const [lockedContinue, setLockedContinue] = useState(false);
+    const [lockedGenerer, setLockedGenerer] = useState(false);
 
-    const actionContinue = () => {setLocked(true)}
+
+    const actionContinue = () => {
+        setLockedContinue(true);
+        setLockedGenerer(true);
+    };
+
+    const actionGenerer = () => {
+        setLockedGenerer(true);
+    };
+
     const actionReset = () => {
         setMaxTables(1);
         setMaxGuests(1);
-        setLocked(false);
+        setLockedContinue(false);
         setName('');
         setErrorFile('');
+        setLockedGenerer(true);
     };
 
     return React.createElement(
@@ -31,9 +44,9 @@ export function Home() {
         { className: 'app-container', style: { padding: '20px', fontFamily: 'sans-serif' } },
         React.createElement('h1', null, '🍞 TOAST 🍞'),
         React.createElement('p', null, 'Tous à sa table'),
-        React.createElement(FileButton, {className: 'file-button', disabled: locked, nameFile: nameFile, setName: setName, errorFile : errorFile, setErrorFile : setErrorFile}),
-        React.createElement(InputNumber, {value: maxGuests, disabled: locked, onChange: val => setMaxGuests(parseInt(val, 10)) }, 'Nombre max de convives par table' ),
-        React.createElement(InputNumber, {value: maxTables, disabled: locked, onChange: val => setMaxTables(parseInt(val, 10)) },'Nombre max de tables'),
+        React.createElement(FileButton, {className: 'file-button', disabled: lockedContinue, nameFile: nameFile, setName: setName, errorFile : errorFile, setErrorFile : setErrorFile}),
+        React.createElement(InputNumber, {value: maxGuests, disabled: lockedContinue, onChange: val => setMaxGuests(parseInt(val, 10)) }, 'Nombre max de convives par table' ),
+        React.createElement(InputNumber, {value: maxTables, disabled: lockedContinue, onChange: val => setMaxTables(parseInt(val, 10)) },'Nombre max de tables'),
 
         React.createElement('div', { style: { marginTop: '20px' } },
             React.createElement(ContinueButton, {
@@ -44,6 +57,8 @@ export function Home() {
               onClick: actionReset
             })
           ),
-        React.createElement(TableColumn,{tableData : tableData, setTableData : setTableData}) //disabled : disabled
-        );
+        React.createElement(TableColumn,{tableData : tableData, setTableData : setTableData, disabled : lockedContinue}),
+        React.createElement(ConflictCenter,{disabled : lockedGenerer}),
+        React.createElement(GenerateButton, {className: 'file-button', onClick: actionGenerer, disabled: !lockedContinue,}),
+    );
 }
