4bf048c6051ebaa7eaa242d2742c9b4d7d4df0db
AUB <roche.aubin@gmail.com>
2025-05-09 10:16:26 +0200

    sending columns to back

--- frontend/src/components/continueButton.js
+++ frontend/src/components/continueButton.js
@@ -5,11 +5,12 @@ import '../App.css';
 
 export function ContinueButton({onClick, disabled}) {
 
+
   return React.createElement(
     'div',
     null,
     React.createElement(
         'button',
         { onClick : onClick, className: 'file-button', disabled : disabled },'Valider' )
-);
+  );
 }
