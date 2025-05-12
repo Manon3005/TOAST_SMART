33acebed136a95da87ee6c05ef603c764ec1d0e4
BugaThis <mathis.getenet@insa-lyon.fr>
2025-05-12 14:55:21 +0200

    Repartition fichier home.js

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -11,7 +11,7 @@ export function NavBar() {
             React.createElement(
                 Link,
                 { to: '/', className: 'nav-link' },
-                'TOAST'
+                'Nouvel événement'
             )
         ),
         React.createElement(
