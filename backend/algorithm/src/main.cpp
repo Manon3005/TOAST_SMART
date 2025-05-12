b879726bf500d1a1866baab8fb82bf00b3cc5a06
AO2708 <a.pitardbouet@gmail.com>
2025-05-12 12:50:27 +0200

    Find error

--- backend/algorithm/src/main.cpp
+++ backend/algorithm/src/main.cpp
@@ -28,11 +28,10 @@ int main(int argc, char* argv[])
     //cout << seatingArrangements; // ICI NON PLUS
 
     ofstream outputFile;
+    cout << seatingArrangements; // ICI CA POSE PROBLEME
     string fileName = "../resources/seatingArrangements.csv";
     outputFile.open(fileName);
-    cout << seatingArrangements; // ICI OUIIIII
     outputFile << seatingArrangements;
-
     return 0;
 }
 
