2c008a2eed44813082219a4f8177af3e49bcc24e
AUB <roche.aubin@gmail.com>
2025-05-13 20:18:28 +0200

    affichage nom fichier import

--- frontend/src/components/exportSolutionButton.js
+++ frontend/src/components/exportSolutionButton.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../App.css';
 
-export function ExportSolutionButton({ onClick, disabled }) {
+export function ExportSolutionButton({ onClick, disabled, nameExportFile, errorExportFile }) {
   return React.createElement(
-    'button',
-    {
-      className: 'export-solution-button',
-      onClick: onClick,
-      disabled: disabled
-    },
-    'Exporter la solution'
+      'div',
+      null,
+      React.createElement('div', { className: 'file-button-container' },
+        React.createElement(
+            'button',
+            { onClick: onClick, className: 'export-solution-button', disabled : disabled},'Exporter la solution' ),
+        
+        React.createElement('div', { className: 'file-message-text' },
+          errorExportFile && React.createElement('p', { style: { color: 'red' } }, errorExportFile),
+          nameExportFile && React.createElement('p', null, '📄 ' + nameExportFile)
+        )
+      ),
   );
 }
