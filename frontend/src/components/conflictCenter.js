4818645a987fd8860a725f8f5862786799f480e8
AUB <roche.aubin@gmail.com>
2025-05-13 09:43:25 +0200

    appel un conflit par un conflit et affichage infos student

--- frontend/src/components/conflictCenter.js
+++ frontend/src/components/conflictCenter.js
@@ -6,36 +6,26 @@ import '../App.css';
 
 
 
-export function ConflictCenter({ students = [], currentStudentIndex = 0, currentNeighbourIndex = 0, onAccept, onRefuse, disabled, fin, onGenerate }) { 
-    const student = students[currentStudentIndex];
-    const neighbour = student?.processedNeighbours?.[currentNeighbourIndex];
+export function ConflictCenter({ student , onAccept, onRefuse, disabled, fin, onGenerate }) { 
 
     if (fin) {
         return React.createElement('div',null,
             React.createElement('p', null, 'Traitement terminé'),
             React.createElement(GenerateButton, {className: 'file-button', onClick: onGenerate}),
-        );
-        
+        );  
     }
-    if (!student || disabled) {
+    if (!student) {
         return React.createElement('p', null, 'Aucun conflit à traiter');
-    }
-    if (!neighbour){
-        return React.createElement('div',null,
-            React.createElement('p', null, 'Pas de voisin.e pour ce diplomé.e'),
-            React.createElement('button', { onClick: onAccept, disabled }, 'Suivant'),
-            React.createElement('p', null, `Cas ${currentStudentIndex + 1}/${students.length}`)
-        );
     }
 
     return React.createElement('div', null,
     React.createElement('p', null, `🎓 Diplômé.e : ${student.firstName} ${student.lastName}`),
-    React.createElement('p', null, `Préférences initiales : ${student.preferedNeighbours}`),
-    React.createElement('p', null, `👤 Voisin.e proposé.e : ${neighbour.neighbourFirstName} ${neighbour.neighbourLastName}`),
+    React.createElement('p', null, `Préférences initiales : ${student.neighboursEntry}`),
+    React.createElement('p', null, `👤 Voisin.e proposé.e : ${student.conflict.neighbourFirstName} ${student.conflict.neighbourLastName}`),
     React.createElement('div', null,
         React.createElement('button', { onClick: onAccept, disabled }, 'Accepter'),
         React.createElement('button', { onClick: onRefuse, disabled }, 'Refuser')
     ),
-    React.createElement('p', null, `Cas ${currentStudentIndex + 1}/${students.length} - Voisin.e ${currentNeighbourIndex + 1}/${student.processedNeighbours.length}`)
+    React.createElement('p', null, `Nombre de conflits restants : ${student.remainingConflictNumber}`)
     );
 }
