085b8885c034ed1d0620b13e964c78d1ef85b43e
AO2708 <a.pitardbouet@gmail.com>
2025-05-14 08:33:46 +0200

    Correction of the stats

--- backend/business/Table.ts
+++ backend/business/Table.ts
@@ -6,22 +6,26 @@ export class Table {
     private nbMaxStudent: number;
     private studentList: GraduatedStudent[] = [];
     private nbStudent: number;
+    private nbFilledSeat: number;
 
     constructor(
         id: string,
         nbMaxStudent: number,
         studentList: GraduatedStudent[],
         nbStudent: number,
+        nbFilledSeat: number,
     ) {
         this.id = id;
         this.nbMaxStudent = nbMaxStudent;
         this.nbStudent = nbStudent;
         this.studentList = studentList;
+        this.nbFilledSeat = nbFilledSeat;
     }
 
     addStudent(student: GraduatedStudent) : void {
         this.studentList.push(student);
-        ++this.nbStudent;
+        this.nbStudent++;
+        this.nbFilledSeat = 1 + student.getGuests().length;
     }
 
     setNbMaxStudent(nbMaxStudent: number): void {
@@ -38,6 +42,10 @@ export class Table {
 
     getNbStudent(): number {
         return this.nbStudent;
+    }
+
+    getNbFilledSeat(): number {
+        return this.nbFilledSeat;
     }
 
     getGraduatedStudents(): GraduatedStudent[] {
