e819272653412f38dbf2d65dec2b7b72f09e84f2
AO2708 <a.pitardbouet@gmail.com>
2025-08-01 23:35:58 +0200

    🔧💄 migration towards tailwind Css

--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -5,18 +5,28 @@ export function Start() {
   const navigate = useNavigate();
 
   return (
-    <div className="start-container bg-red-300">
-      <div className="start-header">
-        <h1 className="page-title">
-          <img src="img/Logo TOAST.png" alt="Logo" className="logo-inline" />
+    <div className="flex flex-1 flex-col items-center gap-[100px]">
+      <div className="w-full flex flex-col items-center mt-[20px] gap-[10px]">
+        <div className="w-full flex flex-row justify-center items-center text-[40px] mt-0 text-black">
+          <img
+            src="img/Logo TOAST.png"
+            alt="Logo"
+            className="w-[50px] h-[50px]"
+          />
           TOus A Sa Table
-          <img src="img/Logo TOAST.png" alt="Logo" className="logo-inline" />
-        </h1>
-        <h3>Réalisez votre plan de table en quelques clics !</h3>
+          <img
+            src="img/Logo TOAST.png"
+            alt="Logo"
+            className="w-[50px] h-[50px]"
+          />
+        </div>
+        <h3 className="text-[1.2rem] text-black">
+          Réalisez votre plan de table en quelques clics !
+        </h3>
       </div>
 
       <Button
-        className="start-button"
+        className="px-[100px] py-[50px] bg-white text-[50px] font-bold text-black font-['Segoe_UI'] outline outline-2 outline-black rounded-[50px] cursor-pointer hover:bg-black hover:text-white hover:outline-white hover:-translate-y-[10px] transition-colors transition-transform duration-300 transform"
         onClick={() => navigate("/preprocessing")}
         disabled={false}
         text="Commencer"
