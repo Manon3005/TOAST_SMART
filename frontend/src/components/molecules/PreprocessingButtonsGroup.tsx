e105e03a9b3a65ce85772780b7feb2ecd3b24913
AO2708 <a.pitardbouet@gmail.com>
2025-09-23 21:40:02 +0200

    💄 change the enable/disabled buttons states

--- frontend/src/components/molecules/PreprocessingButtonsGroup.tsx
+++ frontend/src/components/molecules/PreprocessingButtonsGroup.tsx
@@ -14,15 +14,16 @@ export const PreprocessingButtonsGroup = ({
   return (
     <div className="flex flex-row justify-center gap-[50px]">
       <Button
-        disabled={nameFile == "" || tableData.some((value) => value === "")}
+        disabled={nameFile === "" || tableData.some((value) => value === "")}
         text="Traiter le CSV"
-        className="py-[12px] px-[24px] bg-[#00c21a] hover:bg-[#00ff00] outline-2 outline-black text-black text-[1rem] rounded-[8px] cursor-pointer whitespace-nowrap	transition-colors transition-transform duration-300 transform hover:-translate-y-[1px]"
+        className="py-[12px] px-[24px] bg-[#00c21a] hover:bg-[#00ff00] disabled:hover:bg-[#00c21a] outline-2 outline-black text-black text-[1rem] rounded-[8px] cursor-pointer whitespace-nowrap transition-transform duration-300 transform hover:-translate-y-[1px] disabled:hover:translate-y-0 disabled:cursor-not-allowed"
         onClick={handleFileProcessing}
       />
       <Button
         onClick={actionReset}
+        disabled={nameFile === ""}
         text="Annuler"
-        className="py-[12px] px-[24px] bg-[#fa5c5c] hover:bg-[#ff0000] outline-2 outline-black text-black text-[1rem] rounded-[8px] cursor-pointer whitespace-nowrap	transition-colors transition-transform duration-300 transform hover:-translate-y-[1px]"
+        className="py-[12px] px-[24px] bg-[#fa5c5c] hover:bg-[#ff0000] disabled:hover:bg-[#fa5c5c] outline-2 outline-black text-black text-[1rem] rounded-[8px] cursor-pointer whitespace-nowrap transition-transform duration-300 transform hover:-translate-y-[1px] disabled:hover:translate-y-0 disabled:cursor-not-allowed"
       />
     </div>
   );
--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -9,7 +9,7 @@ export function Start() {
     <div className="flex flex-1 flex-col items-center gap-[100px]">
       <AppHeader />
       <Button
-        className="px-[100px] py-[50px] bg-white text-[50px] font-bold text-black outline outline-2 outline-black rounded-[50px] cursor-pointer hover:bg-black hover:text-white hover:outline-white hover:-translate-y-[10px] transition-colors transition-transform duration-300 transform"
+        className="px-[100px] py-[50px] bg-white text-[50px] font-bold text-black outline outline-2 outline-black rounded-[50px] cursor-pointer hover:bg-black hover:text-white hover:outline-white hover:-translate-y-[10px] transition-transform duration-300 transform"
         onClick={() => navigate("/preprocessing")}
         disabled={false}
         text="Commencer"
